8e3f5eaaa0ed9c48c88cbd9701afce70cc9f93b1
susan <xuxaan5@gmail.com>
2019-04-28 07:19:52 +0545

    multer task

---
+++ Configs/dbconfiguration.js
@@ -0,0 +1,18 @@
+
+var Sequelize = require('sequelize');
+console.log(Sequelize);
+// var sequelize = new Sequelize('pasteboard','root','',{
+// 	host : 'localhost',
+// 	dialect: 'mysql'
+// });
+// sequelize.authenticate()
+// .then(function (){
+// console.log('db connected');
+// })
+// .catch(function(err)
+// {
+
+// })
+// console.log(' jump');
+
+// module.exports
